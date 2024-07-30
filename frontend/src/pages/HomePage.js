0af2a12e7d61a2b663dd9d7922144d98d711670c
RindiyaniSaputriAgu <indiagu03@gmail.com>
2024-07-30 23:24:37 +0800

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+    return (
+        <div className="home-page">
+            <h1>Selamat Datang di Sistem Pemesanan Hotel</h1>
+            <p>Temukan kamar terbaik dan buat pemesanan dengan mudah.</p>
+            <div className="homepage-links">
+                <ul>
+                    <li><Link to="/rooms">Lihat Daftar Kamar</Link></li>
+                    <li><Link to="/booking">Buat Pemesanan</Link></li>
+                    <li><Link to="/bookings">Daftar Pemesanan Anda</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
 
