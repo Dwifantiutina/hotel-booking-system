0489dd72559f6e67be60b5a826b9a25d10fc6e63
RindiyaniSaputriAgu <indiagu03@gmail.com>
2024-07-30 22:48:07 +0800

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,1 +1,21 @@
+const jwt = require('jsonwebtoken');
+const keys = require('./keys');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.header('Authorization') ? req.header('Authorization').replace('Bearer ', '') : null;
+
+    if (!token) {
+        return res.status(401).send('Access Denied');
+    }
+
+    try {
+        const verified = jwt.verify(token, keys.secretOrKey);
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(400).send('Invalid Token');
+    }
+};
+
+module.exports = authMiddleware;
 
